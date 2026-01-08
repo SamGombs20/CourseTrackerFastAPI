25db67e3ecbf7e8c223f80d26a97dd7b467f2253
SamGombs20 <gombajosh18@gmail.com>
2026-01-08 15:48:11 +0300

    Update:New database url

--- app/database.py
+++ app/database.py
@@ -10,7 +10,7 @@ connect_args = {}
 if "neon.tech" in DATABASE_URL:
     connect_args["ssl"] = True
 
-engine = create_async_engine(DATABASE_URL, echo=False, future=True, connect_args=connect_args)
+engine = create_async_engine(DATABASE_URL.split("?")[0], echo=False, future=True, connect_args=connect_args)
 
 async def init_db():
     async with engine.begin() as conn:
